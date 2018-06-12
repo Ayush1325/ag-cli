5839af2ee9bc8a192639dda2be323724784bce5d
Ayush Singh <ayushsingh1325@gmail.com>
2018-06-13 00:38:43 +0530

    feat: create --app can now create all the required files without the content.

--- ad/create.py
+++ ad/create.py
@@ -1,8 +1,17 @@
 import click
+import os
 
 
 def app_create(app):
     click.echo("Creating app %s ..." % app)
+    os.mkdir("%s" % app)
+    with open("{0}/{1}.html".format(app, app), "w") as html_file:
+        html_file.write("")
+    with open("{0}/{1}.css".format(app, app), "w") as css_file:
+        css_file.write("")
+    with open("{0}/{1}.dart".format(app, app), "w") as dart_file:
+        dart_file.write("")
+    click.echo("App %s Created" % app)
 
 
 def comp_create(comp):
