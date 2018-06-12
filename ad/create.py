ac39dfae2c5992a51b09409183dcfdfefb354cd9
Ayush Singh <ayushsingh1325@gmail.com>
2018-06-12 23:39:34 +0530

    feat: Added Click as a dependency.

--- ad/create.py
+++ ad/create.py
@@ -2,8 +2,8 @@ import click
 
 
 def app_create(app):
-    click.echo("Creating app %s" % app)
+    click.echo("Creating app %s ..." % app)
 
 
 def comp_create(comp):
-    click.echo("Creating component %s" % comp)
+    click.echo("Creating component %s ..." % comp)
--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ setup(
     name='ad',
     version='0.0.1',
     packages=['ad'],
+    install_requires=[
+        'Click',
+    ],
     entry_points={
         'console_scripts': [
             'ad = ad.__main__:main'
