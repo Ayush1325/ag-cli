205f7ec12ec5286f3c5f0e47f504422d8f907109
Ayush Singh <ayushsingh1325@gmail.com>
2018-06-13 09:42:58 +0530

    style: made path variable in component_create.

--- ad/component_create.py
+++ ad/component_create.py
@@ -4,11 +4,12 @@ import os
 
 def comp_create(name, path):
     click.echo("Creating component %s ..." % name)
+    file = "{0}/{1}/{2}".format(path, name, name)
     os.mkdir("{0}/{1}".format(path, name))
-    with open("{0}/{1}/{2}.html".format(path, name, name), "w") as html_file:
+    with open("%s.html" % file, "w") as html_file:
         html_file.write("")
-    with open("{0}/{1}/{2}.css".format(path, name, name), "w") as css_file:
+    with open("%s.css" % file, "w") as css_file:
         css_file.write("")
-    with open("{0}/{1}/{2}.dart".format(path, name, name), "w") as dart_file:
+    with open("%s.dart" % file, "w") as dart_file:
         dart_file.write("")
     click.echo("Component %s Created" % name)
