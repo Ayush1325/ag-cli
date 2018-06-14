c16075b7eb6c59ab58e0a8d473c4a974f75f407f
Ayush Singh <ayushsingh1325@gmail.com>
2018-06-15 00:39:55 +0530

    feat: Can now access --help by --h.

--- ad/__main__.py
+++ ad/__main__.py
@@ -9,8 +9,11 @@ import subprocess
 from .app_create import app_create
 from .component_create import comp_create
 
+# Alternate -h parameter for --help.
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
-@click.group()
+
+@click.group(context_settings=CONTEXT_SETTINGS)
 def main():
     pass
 
@@ -19,10 +22,11 @@ def main():
 
 @main.command()
 @click.option("--name", "-n", help="Create App.", required=True)
-@click.option("--path", "-p", default="", help="Path for creating the app.")
-@click.option("--author", "-a", default=getpass.getuser(), help="Author Name.")
-@click.option("--pubget", "-pg", is_flag=True, help="Run pub get after generating the project.")
+@click.option("--path", "-p", default="", help="Path for creating the app.(*Optional)")
+@click.option("--author", "-a", default=getpass.getuser(), help="Author Name.(*Optional)")
+@click.option("--pubget", "-pg", is_flag=True, help="Run pub get after generating the project.(*Optional)")
 def app(name, path, author, pubget):
+    """To Create App."""
     click.echo("Creating name %s ..." % name)
     app_create(name, path, inflection.humanize(author))
     click.echo("App %s Created" % name)
@@ -32,12 +36,13 @@ def app(name, path, author, pubget):
 # Command to generate angular dart component.
 
 
-@main.command()
+@main.command("comp")
 @click.option("--name", "-n", help="Create Component.", required=True)
-@click.option("--path", "-p",  default="lib/src", help="Path for creating the Component.")
+@click.option("--path", "-p",  default="lib/src", help="Path for creating the Component.(*Optional)")
 @click.option("--classname", "-c", default="", help="Class Name of Component.(*Optional)")
 @click.option("--selector", "-s", default="", help="Selector Name of Component.(*Optional)")
-def comp(name, path, classname, selector):
+def component(name, path, classname, selector):
+    """To Create Component."""
     click.echo("Creating component %s ..." % name)
     comp_create(inflection.underscore(name), path,
                 (lambda c: c if c else inflection.camelize(name))(classname),
@@ -48,9 +53,11 @@ def comp(name, path, classname, selector):
 
 @main.command()
 def serve():
+    """To Serve The App."""
     subprocess.run('webdev serve', shell=True)
 
 
 @main.command()
 def build():
+    """To Build The App."""
     subprocess.run('webdev build', shell=True)
