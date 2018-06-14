c60a3436b6a8f474f520ac8622a3c0638f82d4cd
Ayush Singh <ayushsingh1325@gmail.com>
2018-06-14 22:07:10 +0530

    style: migrated skeleton variable data in component_create to files_data.

--- ad/component_create.py
+++ ad/component_create.py
@@ -9,24 +9,19 @@ def comp_create(name, path, class_name, selector):
     skeleton_files = "templates/component"
     filepath = pkg_resources.resource_filename(__name__, skeleton_files)
     files = ["%s.html" % file, "%s.css" % file, "%s.dart" % file]
-    hash = {
-        "%s.html" % file: {},
-        "%s.css" % file: {},
-        "%s.dart" % file: {
+    files_data = {
+        "%s.html" % file: ["component.html.mustache", {}],
+        "%s.css" % file: ["component.css.mustache", {}],
+        "%s.dart" % file: ["component.dart.mustache", {
             'selector': selector,
             'stylesheet': name,
             'html_template': name,
             'directives': 'CORE_DIRECTIVES',
             'class': class_name
-        }
-    }
-    skeleton = {
-        "%s.html" % file: "component.html.mustache",
-        "%s.css" % file: "component.css.mustache",
-        "%s.dart" % file: "component.dart.mustache"
+        }]
     }
     for file in files:
         generate("{0}/{1}".format(path, name), file,
-                 filepath, skeleton[file], hash[file])
+                 filepath, files_data[file][0], files_data[file][1])
 
-    del skeleton, hash, files
+    del files_data, files
