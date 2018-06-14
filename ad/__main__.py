385e438ff1a3402a469182312fae5092182ed6f1
Ayush Singh <ayushsingh1325@gmail.com>
2018-06-15 00:26:07 +0530

    feat: Can now build and serve from ad.

--- ad/__main__.py
+++ ad/__main__.py
@@ -38,13 +38,19 @@ def app(name, path, author, pubget):
 @click.option("--classname", "-c", default="", help="Class Name of Component.(*Optional)")
 @click.option("--selector", "-s", default="", help="Selector Name of Component.(*Optional)")
 def comp(name, path, classname, selector):
-    # if classname:
-    #     comp_create(name, path, classname)
-    # else:
-    #     comp_create(name, path, name)
     click.echo("Creating component %s ..." % name)
     comp_create(inflection.underscore(name), path,
                 (lambda c: c if c else inflection.camelize(name))(classname),
                 (lambda s: s if s else inflection.dasherize(inflection.underscore(name)))(selector))
 
     click.echo("Component %s Created" % name)
+
+
+@main.command()
+def serve():
+    subprocess.run('webdev serve', shell=True)
+
+
+@main.command()
+def build():
+    subprocess.run('webdev build', shell=True)
